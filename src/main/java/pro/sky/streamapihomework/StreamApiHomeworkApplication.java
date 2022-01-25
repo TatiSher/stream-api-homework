cd0da3d71e460cb1bf0885bd6cbc097db7fa4f50
Tatiana <tanja6122212@list.ru>
2022-01-26 02:38:51 +0900

    Initial commit

---
+++ src/main/java/pro/sky/streamapihomework/StreamApiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.streamapihomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/streamapihomework/StreamApiHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.streamapihomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
